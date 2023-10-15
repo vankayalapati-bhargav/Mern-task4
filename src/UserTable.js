b8425d90b2e658a779acd7aa32b45e02178854ee
vankayalapati-bhargav <vankayalapatibhargav2@gmail.com>
2023-10-15 16:14:51 +0530

    first commit

--- src/UserTable.js
+++ src/UserTable.js
@@ -1,61 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-// import 'D:/MERN INTERNSHIP/rtask4/src/UserTable.css'; 
+import './UserTable.css';
 
-
-const UserTable = () => {
-  const [userData, setUserData] = useState([]);
+function UserTable() {
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios.get('https://dummyjson.com/users')
-      .then(response => {
-        const users = response.data.users;
-        setUserData(users);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+    // Fetch data from the provided URL
+    fetch('https://dummyjson.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract and filter the relevant columns
+        const filteredData = data.users.map((user) => ({
+          Sno: user.id,
+          ProfilePic: user.image,
+          Firstname: user.firstName,
+          Lastname: user.lastName,
+          Gender: user.gender,
+          Email: user.email,
+          Username: user.username,
+          Domain: user.domain,
+          IP: user.ip,
+          University: user.university,
+        }));
+        setUsers(filteredData);
       });
   }, []);
 
   return (
-    <div style={{backgroundColor:'black',color:'white',padding:'10px'}}>
-      <h1>Dummy Data</h1>
-      <table className="custom-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>FisrtName</th>
-            <th>LastName</th>
-            <th>Age</th>
-            <th>Email</th>
-            <th>Gender</th>
-            <th>Username</th>
-            <th>Domain</th>
-            <th>IP</th>
-            <th>Univeristy</th>
-            
-          </tr>
-        </thead>
-        <tbody>
-          {userData.map(user => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.firstName}</td>
-              <td>{user.lastName}</td>
-              <td>{user.age}</td>
-              <td>{user.email}</td>
-              <td>{user.gender}</td>
-              <td>{user.username}</td>
-              <td>{user.domain}</td>
-              <td>{user.ip}</td>
-              <td>{user.university}</td>
-
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className='body'>
+        <div >
+            <h2>Dummy Data</h2>
+            <div className='table'>
+                <table >
+                    <thead >
+                    <tr>
+                        <th>Sno</th>
+                        <th>ProfilePic</th>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Gender</th>
+                        <th>Email</th>
+                        <th>Username</th>
+                        <th>Domain</th>
+                        <th>IP</th>
+                        <th>University</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {users.map((user) => (
+                        <tr key={user.Sno}>
+                        <td>{user.Sno}</td>
+                        <td className="profile-pic-cell" ><img src={user.ProfilePic} alt={`Profile of ${user.Firstname}`} /></td>
+                        <td>{user.Firstname}</td>
+                        <td>{user.Lastname}</td>
+                        <td>{user.Gender}</td>
+                        <td>{user.Email}</td>
+                        <td>{user.Username}</td>
+                        <td>{user.Domain}</td>
+                        <td>{user.IP}</td>
+                        <td>{user.University}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
   );
-};
+}
 
 export default UserTable;
